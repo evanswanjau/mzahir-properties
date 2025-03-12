95e83f7ab176b12a6797724b10c64ec58dab9efa
evanswanjau <evanswanjau@gmail.com>
2025-03-12 19:50:17 +0300

    Reduced text size and added services link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,16 @@ import { Button } from "@/components/ui/button";
 const Header = () => {
   const navLinks = [
     { name: "Home", href: "/" },
-    { name: "Buy Property", href: "#buy-property" },
-    { name: "Sell Property", href: "#sell-property" },
+    { name: "Buy Property", href: "/buy-property" },
+    { name: "Sell Property", href: "/sell-property" },
+    { name: "Our Services", href: "/our-services" },
     { name: "About", href: "/about" },
     { name: "Blog", href: "/blog" },
     { name: "FAQs", href: "/faqs" },
   ];
 
   return (
-    <header className="bg-white text-gray-800 p-4 flex justify-between items-center px-54 py-4">
+    <header className="bg-white text-gray-800 p-4 flex justify-between items-center px-42 py-4">
       <div className="flex items-center">
         <a href="/">
           <img
@@ -28,20 +29,17 @@ const Header = () => {
             <li key={link.name}>
               <a
                 href={link.href}
-                className="text-gray-600 hover:text-green-700 transition-colors duration-200 text-lg"
+                className="text-gray-600 hover:text-green-700 transition-colors duration-200"
               >
                 {link.name}
               </a>
             </li>
           ))}
           <li>
-            <a
-              href="/contact-us"
-              className="text-gray-600 hover:text-green-700 transition-colors duration-200 text-lg"
-            >
+            <a href="/contact-us">
               <Button
                 variant="default"
-                className="text-lg cursor-pointer py-6 px-6 bg-green-700 text-white hover:bg-green-800 transition-colors duration-300"
+                className="cursor-pointer py-6 px-6 bg-green-700 text-white hover:bg-green-800 transition-colors duration-300"
               >
                 Get in touch
               </Button>
