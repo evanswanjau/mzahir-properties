d0814504fc7877f7ffd03aa327cb13484ab2d3ca
evanswanjau <evanswanjau@gmail.com>
2025-03-12 22:52:22 +0300

    Redesigned Blog Card for equal height

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -8,18 +8,20 @@ interface BlogPost {
 
 export const BlogCard = ({ post }: { post: BlogPost }) => {
   return (
-    <div className="bg-white rounded-2xl p-4 hover:shadow-2xl transition-shadow duration-500 cursor-pointer">
+    <div className="bg-white rounded-2xl p-4 hover:shadow-2xl transition-shadow duration-500 cursor-pointer flex flex-col min-h-[400px]">
       <img
         src={post.image}
         alt={post.title}
         className="w-full h-42 object-cover rounded-2xl mb-5"
       />
-      <div>
-        <h3 className="text-xl font-semibold text-gray-800 mb-2">
-          {post.title}
-        </h3>
-        <p className="text-gray-500 mb-4">{post.excerpt}</p>
-        <div className="flex justify-end items-center text-gray-500">
+      <div className="flex flex-col flex-1 justify-between">
+        <div>
+          <h3 className="text-xl font-semibold text-gray-800 mb-2">
+            {post.title}
+          </h3>
+          <p className="text-gray-500 mb-4">{post.excerpt}</p>
+        </div>
+        <div className="flex justify-end items-center text-gray-500 mt-auto">
           <span>{post.datePosted}</span>
         </div>
       </div>
